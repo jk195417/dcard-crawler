275092dadf7ee68a21f7b132bf1e5dfbfb3ed85e
楊竑昕 <jk195417@gmail.com>
2018-11-28 10:34:19 +0800

    更新 Dcard API 加入時間排序與 before after 等參數

--- services/dcard_api.rb
+++ services/dcard_api.rb
@@ -2,32 +2,36 @@ class DcardAPI
   @@root = 'https://www.dcard.tw/_api'
 
   def self.forums
-    @@root+'/forums'
+    "#{@@root}/forums"
   end
 
   def self.forum(name)
-    @@root + "/forums/#{name}"
+    "#{@@root}/forums/#{name}"
   end
 
-  def self.forum(name)
-    @@root + "/forums/#{name}"
+  def self.forum_posts(name, before: nil, popular: false)
+    link = "#{@@root}/forums/#{name}/posts?limit=100&popular=#{popular}"
+    link += "&before=#{before}" unless before.nil?
+    link
   end
 
-  def self.forum_posts(name)
-    @@root + "/forums/#{name}/posts"
+  def self.posts(before: nil, popular: false)
+    link = "#{@@root}/posts/?limit=100&popular=#{popular}"
+    link += "&before=#{before}" unless before.nil?
+    link
   end
 
-  def self.posts(pid: nil)
-    node = '/posts/'
-    node += "/#{pid}" if pid
-    @@root + node
+  def self.post(pid)
+    "#{@@root}/posts/#{pid}"
   end
 
   def self.post_links(pid)
-    @@root + "/posts/#{pid}/links"
+    "#{@@root}/posts/#{pid}/links"
   end
 
-  def self.post_comments(pid)
-    @@root + "/posts/#{pid}/comments"
+  def self.post_comments(pid, after: nil)
+    link = "#{@@root}/posts/#{pid}/comments"
+    link = "#{@@root}/posts/#{pid}/comments&after=#{after}" unless after.nil?
+    link
   end
 end
