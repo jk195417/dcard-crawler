7c6e1cccd6f7bc06c1553b0d2fa05f8316d13d10
楊竑昕 <jk195417@gmail.com>
2018-12-05 12:02:38 +0800

    用 Transaction 把插入留言與更新 comments_count 包起來

--- app/actions/get_posts_comments.rb
+++ app/actions/get_posts_comments.rb
@@ -5,18 +5,21 @@ module App::Actions
         begin
           import_comments = []
           latest_floor = post.comments.order(floor: :desc).first&.floor
+
           loop do
             new_comments = post.new_comments_from_dcard(after: latest_floor)
-            break unless new_comments.is_a?(Array)
-            break if new_comments.empty?
+            break if new_comments.blank?
             import_comments += new_comments
             latest_floor = new_comments.max { |a, b| a[:floor].to_i <=> b[:floor].to_i }[:floor]
           end
-          results = Comment.import import_comments
-          results_count = results.ids.size
-          if results_count > 0
-            Post.update_counters(post.id, comments_count: results_count)
-            App.logger.info "Post #{post.dcard_id} get #{results_count} new comments and inserted into database"
+
+          import_comments_size = import_comments.size
+          if import_comments_size > 0
+            Post.transaction do
+              Comment.import import_comments
+              Post.update_counters(post.id, comments_count: import_comments_size)
+            end
+            App.logger.info "Post #{post.dcard_id} get #{import_comments_size} new comments and inserted into database"
           end
         rescue => e
           App.logger.error e.inspect
