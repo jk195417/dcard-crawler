46085dd4e1b60c2262d09336a8808d90f182a610
楊竑昕 <jk195417@gmail.com>
2018-12-21 15:25:30 +0800

    get_forums_posts_comments 改名為 get_posts 並新增 rake task

--- Rakefile
+++ Rakefile
@@ -9,6 +9,11 @@ task run: :environment do
 end
 
 namespace :run do
+  desc '爬取非學校論壇的貼文'
+  task get_posts: :environment do
+    App::Actions.get_posts
+  end
+
   desc '爬取還未被刪除且留言數大於0的貼文留言'
   task get_comments: :environment do
     App::Actions.get_comments
--- app/actions/get_posts.rb
+++ app/actions/get_posts.rb
@@ -1,7 +1,7 @@
 module App::Actions
   extend self
-  def get_forums_posts(recent: false, popular: false)
-    Forum.find_each do |forum|
+  def get_posts(recent: false, popular: false)
+    Forum.where(is_school: false).find_each do |forum|
       PostsCrawler.perform_async(forum.id, recent, popular)
     end
   end
