6e65b5c040db5237902e71601581ce4faff8b21d
jk195417 <jk195417gmail.com>
2019-02-13 15:24:42 +0800

    using array instead of ActiveRecord object

--- app/workers/get_comments_embeddings.rb
+++ app/workers/get_comments_embeddings.rb
@@ -2,18 +2,29 @@ class GetCommentsEmbeddings
   include Sidekiq::Worker
 
   def perform(first_comment_id, last_comment_id)
-    comments = Comment.select(:id, :content).where(id: (first_comment_id..last_comment_id), embedding: nil).where.not(content: [nil, ''])
-    comments_content = comments.map(&:content)
+    comments = Comment.where(id: (first_comment_id..last_comment_id), embedding: nil).where.not(content: [nil, '']).pluck(:id, :content)
     return if comments.blank?
 
+    comment_ids = []
+    comment_contents = []
+    comments.each do |comment|
+      comment_ids << comment[0]
+      comment_contents << comment[1]
+    end 
+
     # get comments embeddings
-    response = $bc.encode(comments_content)
-    comments.each.with_index do |comment, i|
-      comment.embedding = PyCall::List.call(response[i]).to_a
+    bc = BertClient.new
+    comment_embeddings = bc.encode(comment_contents)
+    bc.close
+
+    data = []
+    comment_ids.each.with_index do |id, index|
+      embedding = PyCall::List.call(comment_embeddings[index]).to_a
+      data << { id: id, embedding: embedding }
     end
 
     # bulk update comments at one sql, like a upsert.
-    Comment.import comments.as_json, on_duplicate_key_update: [:embedding]
+    Comment.import data, on_duplicate_key_update: [:embedding]
     App.logger.info "Update comments embeddings between #{first_comment_id} and #{last_comment_id}."
   end
 end
