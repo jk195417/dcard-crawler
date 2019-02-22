0f0f78ce0555e4885f9ec38e595a86c1e1b13c95
jk195417 <jk195417gmail.com>
2019-02-22 15:23:23 +0800

    update actions

--- app/actions/get_comments.rb
+++ app/actions/get_comments.rb
@@ -4,5 +4,8 @@ module App::Actions
     Post.not_removed.where('comment_count > 0').where(comments_count: 0).find_each do |post|
       CommentsCrawler.perform_async(post.id)
     end
+    Post.not_removed.where('comment_count != comments_count').find_each do |post|
+      CommentsCrawler.perform_async(post.id)
+    end
   end
 end
--- app/actions/get_comments_embeddings.rb
+++ app/actions/get_comments_embeddings.rb
@@ -3,7 +3,7 @@ module App::Actions
   def get_comments_embeddings
     # batch update comment's embedding, each time update 256 comments.
     batch_size = 256
-    Comment.select(:id).where('id < 1000000').where(embedding: nil).where.not(content: [nil, '']).find_in_batches(batch_size: batch_size) do |comments|
+    Comment.select(:id).where(embedding: nil).where.not(content: [nil, '']).find_in_batches(batch_size: batch_size) do |comments|
       GetCommentsEmbeddings.perform_async(comments.first.id, comments.last.id)
     end
   end
