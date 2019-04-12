20f677f777c7f46da7f58095fd1b119aa4011448
楊竑昕 <jk195417@gmail.com>
2019-04-12 20:07:44 +0800

    bootstrap_alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_alert_class(type)
+    alert_class_for = {
+      notice: 'success',
+      alert: 'danger'
+    }
+    "alert-#{alert_class_for.fetch(type.to_sym, 'primary')}"
+  end
+
+  def bootstrap_alert(message, type = 'notice')
+    icon = content_tag :i, '', class: 'fas fa-times'
+    close_btn = content_tag :button, icon, class: 'close', 'data-dismiss': 'alert'
+    content = message + close_btn
+    content_tag(:div, content.html_safe, class: "alert #{bootstrap_alert_class(type)} alert-dismissible fade show").html_safe
+  end
 end
