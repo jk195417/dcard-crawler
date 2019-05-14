2789e7a6b6e31f6a640f9b0cfa7b9ce701c62bee
jk195417 <jk195417gmail.com>
2019-05-14 13:03:39 +0800

    Update job, don't mess up counter cache column

--- app/jobs/dcard/update_forums_job.rb
+++ app/jobs/dcard/update_forums_job.rb
@@ -5,7 +5,7 @@ class Dcard::UpdateForumsJob < ApplicationJob
     forums = Dcard::Forum.to_records(Dcard::Forum.get)
     result = ::Forum.import(forums, on_duplicate_key_update: {
                               conflict_target: [:alias],
-                              columns: ::Forum.column_names - %w[id alias created_at updated_at]
+                              columns: ::Forum.column_names - %w[id alias posts_count created_at updated_at]
                             })
     Rails.logger.info "Forum #{result.ids} updated."
   end
