f95a7cbfbdcc1ee66d7db34ac449d5d863683a16
jk195417 <jk195417gmail.com>
2019-05-15 17:07:20 +0800

    Update sentiment jobs

--- app/jobs/baidu/get_comment_sentiment_job.rb
+++ app/jobs/baidu/get_comment_sentiment_job.rb
@@ -6,6 +6,22 @@ class Baidu::GetCommentSentimentJob < ApplicationJob
     comment = Comment.find id
     text = comment.content&.gsub(URI::DEFAULT_PARSER.make_regexp, '') # remove url
     Rails.logger.info { "Analyzing sentiment of Comment #{comment.floor} of Post #{comment.post_dcard_id}" }
-    Baidu::Aip.new.sentiment(text)
+    result = Baidu::Aip.new.sentiment(text)
+    Rails.logger.info { "Can\'t get sentiment of Comment #{comment.floor} of Post #{comment.post_dcard_id}" } && return if result.blank?
+
+    if comment.sentiment.present?
+      comment.sentiment.positive = result['positive_prob']
+      comment.sentiment.negative = result['negative_prob']
+      comment.sentiment.confidence = result['confidence']
+      comment.sentiment.sentiment = result['sentiment']
+    else
+      comment.create_sentiment!(positive: result['positive_prob'],
+                              negative: result['negative_prob'],
+                              confidence: result['confidence'],
+                              sentiment: result['sentiment'])
+    end
+    comment.save!
+    sleep(1)
+    Rails.logger.info { "Sentiment of Comment #{comment.floor} of Post #{comment.post_dcard_id} is analyzed" }
   end
 end
--- app/jobs/baidu/get_post_sentiment_job.rb
+++ app/jobs/baidu/get_post_sentiment_job.rb
@@ -6,9 +6,24 @@ class Baidu::GetPostSentimentJob < ApplicationJob
     post = Post.find id
     text = post.content&.gsub(URI::DEFAULT_PARSER.make_regexp, '') # remove url
     Rails.logger.info { "Analyzing sentiment of Post #{post.dcard_id}" }
-    Baidu::Aip.new.sentiment(text)
-    post.comments.find_each do |comment|
-      Baidu::GetCommentSentimentJob.perform_now(comment.id)
+    result = Baidu::Aip.new.sentiment(text)
+    if post.sentiment.present?
+      post.sentiment.positive = result['positive_prob']
+      post.sentiment.negative = result['negative_prob']
+      post.sentiment.confidence = result['confidence']
+      post.sentiment.sentiment = result['sentiment']
+    else
+      post.build_sentiment(positive: result['positive_prob'],
+                           negative: result['negative_prob'],
+                           confidence: result['confidence'],
+                           sentiment: result['sentiment'])
     end
+    post.save!
+    Rails.logger.info { "Sentiment of Post #{post.dcard_id} is analyzed" }
+    post.comments.find_each do |comment|
+      Baidu::GetCommentSentimentJob.perform_later(comment.id)
+    end
+    sleep(1)
+    Rails.logger.info { "Scheduling analyze sentiment of comments, which belongs to post #{post.dcard_id} " }
   end
 end
