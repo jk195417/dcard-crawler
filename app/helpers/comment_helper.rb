d7b3d92b1da9f348dba23d318f41bb177cbe3283
楊竑昕 <jk195417@gmail.com>
2019-05-17 18:28:16 +0800

    Rename node_html to dcard_html and moving to CommentHelper

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -6,4 +6,38 @@ module CommentHelper
       end
     end.html_safe
   end
+  
+  def gender_color(gender, opacity: 1)
+    if gender == 'M'
+      "rgba(102,212,253,#{opacity})"
+    elsif gender == 'F'
+      "rgba(255,129,173,#{opacity})"
+    else
+      "rgba(25,25,25,#{opacity})"
+    end
+  end
+
+  def dcard_html(post_or_comment)
+    "<div id=\"floor-#{post_or_comment&.floor}\"
+          class=\"card mb-2\"
+          style=\"background-color: #{gender_color(post_or_comment&.gender, opacity: 0.4)}\">
+      <div class=\"card-body\">
+        <div class=\"card-title\">
+          <h5>#{post_or_comment&.school} #{post_or_comment&.department}</h5>
+          <p class=\"text-muted\">B#{post_or_comment&.floor} #{time_formatter(post_or_comment&.created_at)}</p>
+        </div>
+        <div class=\"card-text\">
+          #{url_to_img simple_format(post_or_comment&.content)}
+        </div>
+        <div class=\"card-text small text-muted\">
+          <span>
+            <i class=\"fas fa-heart\" style=\"color: rgb(254,69,78)\"></i>
+            <i class=\"fas fa-times\"></i>
+            #{post_or_comment&.like_count}
+          </span>
+          #{sentiment_span post_or_comment&.sentiment, klass: 'ml-2'}
+        </div>
+      </div>
+    </div>".html_safe
+  end
 end
--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -1,37 +1,4 @@
-# frozen_string_literal: true
-
 module GraphHelper
-  def gender_color(gender, opacity: 1)
-    if gender == 'M'
-      "rgba(102,212,253,#{opacity})"
-    elsif gender == 'F'
-      "rgba(255,129,173,#{opacity})"
-    else
-      "rgba(25,25,25,#{opacity})"
-    end
-  end
-
-  def node_html(post_or_comment)
-    "<div id=\"floor-#{post_or_comment.floor}\"
-          class=\"card\"
-          style=\"background-color: #{gender_color(post_or_comment.gender, opacity: 0.4)}\">
-      <div class=\"card-body\">
-        <div class=\"card-title\">
-          <h5>#{post_or_comment.school} #{post_or_comment.department}</h5>
-          <p class=\"text-muted\">B#{post_or_comment.floor} #{time_formatter(post_or_comment.created_at)}</p>
-        </div>
-        <div class=\"card-text\">
-          #{url_to_img(simple_format(post_or_comment.content))}
-        </div>
-        <div class=\"card-text small text-muted\">
-          <i class=\"fas fa-heart\" style=\"color: rgb(254,69,78)\"></i>
-          <i class=\"fas fa-times\"></i>
-          #{post_or_comment.like_count}
-        </div>
-      </div>
-    </div>"
-  end
-
   def force_graph_data_formater(post, comments)
     result = {
       nodes: [
