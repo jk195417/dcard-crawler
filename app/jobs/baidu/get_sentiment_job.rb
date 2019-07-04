a37c54de45fd2a0de3f1e4ee75fd11dc34ffbd88
jk195417 <jk195417gmail.com>
2019-07-05 00:58:16 +0800

    Update Baidu::GetSentimentJob

--- app/jobs/baidu/get_sentiment_job.rb
+++ app/jobs/baidu/get_sentiment_job.rb
@@ -5,16 +5,24 @@ class Baidu::GetSentimentJob < ApplicationJob
   queue_as :baidu
 
   def perform(post_or_comment)
-    text = post_or_comment.content&.gsub(URI::DEFAULT_PARSER.make_regexp, '') # remove url
+    text = content_filter(post_or_comment.content)
+    sentiment = Sentiment.find_or_initialize_by(sentimental: post_or_comment)
     result = Baidu::Aip.new.sentiment(text)
     Rails.logger.error { "Can\'t get sentiment of #{post_or_comment}" } && return if result.blank?
 
-    Sentiment.find_or_create_by(sentimental: post_or_comment) do |sentiment|
-      sentiment.positive = result['positive_prob']
-      sentiment.negative = result['negative_prob']
-      sentiment.confidence = result['confidence']
-      sentiment.sentiment = result['sentiment']
-    end
+    sentiment.positive = result['positive_prob']
+    sentiment.negative = result['negative_prob']
+    sentiment.confidence = result['confidence']
+    sentiment.sentiment = result['sentiment']
+    sentiment.save!
     sleep(1)
+  end
+
+  private
+
+  def content_filter(content)
+    raise 'no content' if content.blank?
+    content = content.gsub(URI::DEFAULT_PARSER.make_regexp, '') # remove url
+    content = content.gsub(/[bB]\d+/, '')  # remove mentions
   end
 end
