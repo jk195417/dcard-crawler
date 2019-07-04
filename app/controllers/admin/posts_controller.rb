f8298773cbee3b0698a498e8b811080afa184f23
楊竑昕 <jk195417@gmail.com>
2019-07-05 00:14:57 +0800

    Update Dcard::UpdatePostJob

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -17,10 +17,10 @@ class Admin::PostsController < Admin::BaseController
     @post.forum = forum
     @alert = {}
     if @post.save
-      Dcard::UpdatePostJob.perform_later(@post.id)
-      @alert[:notice] = "Crawling comments of post #{@post.id}, you can refresh your browser to read it."
+      Dcard::UpdatePostJob.perform_later(@post)
+      @alert[:notice] = "Crawling comments of Post #{@post.dcard_id}, you can refresh your browser to read it."
     else
-      @alert[:alert] = "Can not crawl this post from #{@post.dcard_url}, maybe you already crawled it."
+      @alert[:alert] = "Can not crawl this Post from #{@post.dcard_url}, maybe you already crawled it."
     end
     respond_to do |format|
       format.html { redirect_back({ fallback_location: admin_posts_path }.merge(@alert)) }
@@ -29,8 +29,8 @@ class Admin::PostsController < Admin::BaseController
   end
 
   def update
-    Dcard::UpdatePostJob.perform_later(@post.id)
-    @alert = { notice: "Crawling comments of post #{@post.id}, you can refresh your browser to read it." }
+    Dcard::UpdatePostJob.perform_later(@post)
+    @alert = { notice: "Crawling comments of Post #{@post.dcard_id}, you can refresh your browser to read it." }
     respond_to do |format|
       format.html { redirect_back({ fallback_location: admin_posts_path }.merge(@alert)) }
       format.js
--- app/jobs/dcard/update_post_job.rb
+++ app/jobs/dcard/update_post_job.rb
@@ -1,20 +1,17 @@
 # Usage :
-# Dcard::UpdatePostJob.perform_now(post.id)
+# Dcard::UpdatePostJob.perform_now(post)
 
 class Dcard::UpdatePostJob < ApplicationJob
   queue_as :default
 
-  def perform(id)
-    post = ::Post.find id
-    post_crawler = Dcard::Post.new(post.dcard_id)
+  def perform(post)
+    crawler = Dcard::Post.new(post.dcard_id)
     begin
-      new_post = post_crawler.to_record # 可能因為被刪除的留言導致 TypeError
+      new_post = crawler.to_record # 可能因為被刪除的留言導致 TypeError
       attributes = new_post.attributes
       %w[id forum_id comments_count reviews_count].each { |key| attributes.delete(key) }
     rescue TypeError
-      if post_crawler.data.fetch('error') { nil }.to_i == 1202
-        attributes = { removed: post_crawler.data.fetch('reason') { 'removed' } }
-      end
+      attributes = { removed: crawler.data.fetch('reason') { 'removed' } } if crawler.data.fetch('error') { nil }.to_i == 1202
     end
     post.update! attributes
     Dcard::GetPostCommentsJob.perform_now(post)
